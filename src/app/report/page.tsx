08adcdbd6454886eef3c8bfdfd8d268f0e7ba136
Jessica Ryan <jessicaryan0419@gmail.com>
2025-08-22 22:53:28 +0700

    feat: add ai card container

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,3 +1,4 @@
+import AIContainerCard from "@/components/cards/ai-container-card";
 import OngoingCallCard from "@/components/cards/ongoing-call-card";
 import ReportFormCard from "@/components/cards/report-form-card";
 
@@ -6,7 +7,7 @@ export default function Report() {
         <div className="flex w-full h-full gap-4">
             <div className="w-1/3 h-full flex flex-col flex-1 py-4 gap-6">
                 <OngoingCallCard />
-
+                <AIContainerCard/>
             </div>
             <div className="w-2/3 h-full py-4 flex flex-col gap-6">
                 <ReportFormCard/>
--- src/components/cards/report-form-card.tsx
+++ src/components/cards/report-form-card.tsx
@@ -1,13 +1,15 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Button } from "../ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
+import { Select } from "@radix-ui/react-select";
+import { SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 export default function ReportFormCard() {
     return (
-        <Card className="h-full">
+        <Card>
             <CardHeader>
                 <CardTitle>
                     <Label type="title">Laporan 25080600316</Label>
@@ -15,63 +17,99 @@ export default function ReportFormCard() {
             </CardHeader>
 
             <CardContent>
-                <div>
+                <div className="flex flex-col gap-4">
                     <Label type="subtitle">Data Pelapor</Label>
                     <div className="w-full flex gap-10">
-                        <div className="w-1/2 flex gap-4">
-                            <Label>Nomor Telepon</Label>
-                            <Input placeholder="Nomor telepon" />
+                        <div className="w-1/2 flex">
+                            <Label className="w-1/3">Nomor Telepon</Label>
+                            <Input className="w-2/3" placeholder="Nomor telepon" />
                         </div>
-                        <div className="w-1/2 flex gap-4">
-                            <Label >Nama</Label>
-                            <Input placeholder="Nama" />
+                        <div className="w-1/2 flex">
+                            <Label className="w-1/3">Nama</Label>
+                            <Input className="w-2/3" placeholder="Nama" />
                         </div>
                     </div>
 
                     <Label type="subtitle">Data Laporan</Label>
                     <div className="w-full flex gap-10 ">
-                        <div className="w-1/2 flex gap-4">
-                            <Label>Jenis Laporan</Label>
-                            <Input placeholder="Nomor telepon" />
+                        <div className="w-1/2 flex">
+                            <Label className="w-1/3">Jenis Laporan</Label>
+                            <Select>
+                                <SelectTrigger className="w-2/3">
+                                    <SelectValue placeholder="Theme" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="light">Light</SelectItem>
+                                    <SelectItem value="dark">Dark</SelectItem>
+                                    <SelectItem value="system">System</SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
-                        <div className="w-1/2 flex gap-4">
-                            <Label >Tipe Kejadian</Label>
-                            <Input placeholder="Nama" />
+                        <div className="w-1/2 flex">
+                            <Label className="w-1/3">Tipe Kejadian</Label>
+                            <Select>
+                                <SelectTrigger className="w-2/3">
+                                    <SelectValue placeholder="Theme" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="light">Light</SelectItem>
+                                    <SelectItem value="dark">Dark</SelectItem>
+                                    <SelectItem value="system">System</SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
                     </div>
 
                     <div className="w-full flex gap-10 ">
-                        <div className="w-1/2 flex gap-4">
-                            <Label>Kecamatan</Label>
-                            <Input placeholder="Nomor telepon" />
+                        <div className="w-1/2 flex">
+                            <Label className="w-1/3">Kecamatan</Label>
+                            <Select>
+                                <SelectTrigger className="w-2/3">
+                                    <SelectValue placeholder="Theme" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="light">Light</SelectItem>
+                                    <SelectItem value="dark">Dark</SelectItem>
+                                    <SelectItem value="system">System</SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
-                        <div className="w-1/2 flex gap-4">
-                            <Label >Kelurahan</Label>
-                            <Input placeholder="Nama" />
+                        <div className="w-1/2 flex">
+                            <Label className="w-1/3">Kelurahan</Label>
+                            <Select>
+                                <SelectTrigger className="w-2/3">
+                                    <SelectValue placeholder="Theme" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="light">Light</SelectItem>
+                                    <SelectItem value="dark">Dark</SelectItem>
+                                    <SelectItem value="system">System</SelectItem>
+                                </SelectContent>
+                            </Select>
                         </div>
                     </div>
 
                     <div className="w-full flex gap-10 ">
-
-                        <Label>Detail Alamat</Label>
+                        <Label className="w-1/6">Detail Alamat</Label>
                         <Textarea placeholder="Detail Alamat" />
                     </div>
 
                     <Label type="subtitle">Data Kejadian</Label>
                     <Textarea placeholder="Detail Kejadian" />
-                    
-                    <div className="flex gap-4">
-                        <Button variant="success">
-                            <Icon icon="ion:call"/>
-                            Hubungi Dispatcher
-                        </Button>
-                        <Button variant="default">
-                            <Icon icon="teenyicons:send-up-outline"/>
-                            Simpan Laporan
-                        </Button>
-                    </div>
                 </div>
             </CardContent>
+            <CardFooter>
+                <div className="flex gap-4">
+                    <Button variant="success">
+                        <Icon icon="ion:call" />
+                        Hubungi Dispatcher
+                    </Button>
+                    <Button variant="default">
+                        <Icon icon="teenyicons:send-up-outline" />
+                        Simpan Laporan
+                    </Button>
+                </div>
+            </CardFooter>
         </Card >
     )
 }
